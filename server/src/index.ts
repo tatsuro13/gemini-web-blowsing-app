03b07b9064fe2ac5f95b7d6e1142f2baeb5b07d7
Tatsuro Osuka <oscarwild13@gmail.com>
2024-11-04 12:02:38 +0900

    CORSとdotenvを追加し、APIエンドポイントの設定を更新

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,24 @@
 import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 2000;
 
+const corsOptions = {
+    origin: 'http://localhost:9000',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get('api/hello', (req:Request, res: Response) => {
+app.get('/api/hello', (req:Request, res: Response) => {
     res.json({ message: 'Hello World!' });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
--- src/components/GeminiForm.tsx
+++ src/components/GeminiForm.tsx
@@ -1,7 +1,7 @@
-import {
-    DynamicRetrievalMode,
-    GoogleGenerativeAI,
-  } from "@google/generative-ai";
+// import {
+//     DynamicRetrievalMode,
+//     GoogleGenerativeAI,
+//   } from "@google/generative-ai";
 import { useState } from "react"
 
 //google geminiの回答を受けるフォーム
@@ -11,43 +11,52 @@ import { useState } from "react"
 
 export const GeminiForm: React.FC = () => {
     const [answer, setAnswer] = useState<string>('')
-    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log('submit')
-    try{
-        const apiKey: string = import.meta.env.VITE_GEMINI_API_KEY as string;
-        const genAI = new GoogleGenerativeAI(apiKey);
-        const model = genAI.getGenerativeModel(
-            {
-                model: "model/gemini-1.5-pro-002",
-                tools:[
-                {
-                    googleSearchRetrieval: {
-                        dynamicRetrievalConfig:{
-                            mode: DynamicRetrievalMode.MODE_DYNAMIC,
-                            dynamicThreshold: 0.7,
-                        }
-                    }      
-                }
-            ],
-        },
-        {apiVersion: "v1beta"}
-        )
+    // const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    // e.preventDefault()
+    // console.log('submit')
+    // try{
+    //     const apiKey: string = import.meta.env.VITE_GEMINI_API_KEY as string;
+    //     const genAI = new GoogleGenerativeAI(apiKey);
+    //     const model = genAI.getGenerativeModel(
+    //         {
+    //             model: "model/gemini-1.5-pro-002",
+    //             tools:[
+    //             {
+    //                 googleSearchRetrieval: {
+    //                     dynamicRetrievalConfig:{
+    //                         mode: DynamicRetrievalMode.MODE_DYNAMIC,
+    //                         dynamicThreshold: 0.7,
+    //                     }
+    //                 }      
+    //             }
+    //         ],
+    //     },
+    //     {apiVersion: "v1beta"}
+    //     )
         
-        const prompt = "What is the capital of Japan?"
-        const result = await model.generateContent(prompt)
+    //     const prompt = "What is the capital of Japan?"
+    //     const result = await model.generateContent(prompt)
 
-        if (result.response && result.response.candidates && result.response.candidates.length > 0) {
-            console.log(result.response.candidates[0].groundingMetadata);
-            const groundingMetadata = result.response.candidates[0].groundingMetadata;
-            setAnswer(groundingMetadata ? JSON.stringify(groundingMetadata) : 'No answer found');
-          } else {
-            console.error('No candidates found in the response.');
-          }
-        } catch (error) {
-          console.error('Error generating content:', error);
-        }
-      };
+    //     if (result.response && result.response.candidates && result.response.candidates.length > 0) {
+    //         console.log(result.response.candidates[0].groundingMetadata);
+    //         const groundingMetadata = result.response.candidates[0].groundingMetadata;
+    //         setAnswer(groundingMetadata ? JSON.stringify(groundingMetadata) : 'No answer found');
+    //       } else {
+    //         console.error('No candidates found in the response.');
+    //       }
+    //     } catch (error) {
+    //       console.error('Error generating content:', error);
+    //     }
+    //   };
+
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log('submit')
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/hello`)
+        const data = await response.json()
+        console.log(data)
+        setAnswer(data.message)
+    }
 
   return (
     <div>
